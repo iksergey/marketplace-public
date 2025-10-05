961be7edf2b888470b020183cc727966e343f49b
iksergey <i@ksergey.ru>
2025-10-05 11:23:06 +0300

    Добавлен кастомный CreateCatalogItemHandler для микросервиса Catalog

---
+++ src/Services/Catalog/Catalog.Application/Handlers/CatalogItemHandlers/CreateCatalogItemHandler.cs
@@ -0,0 +1,50 @@
+namespace Catalog.Application.Handlers.CatalogItemHandlers;
+
+public class CreateCatalogItemHandler(ICatalogItemRepository catalogItemRepository)
+    : IRequestHandler<CreateCatalogItemCommand, CreateCatalogItemResult>
+{
+    public async Task<CreateCatalogItemResult> Handle(CreateCatalogItemCommand command,
+        CancellationToken cancellationToken)
+    {
+        var catalogItem = command.Adapt<CatalogItem>();
+        catalogItem.Id = Guid.NewGuid();
+        await catalogItemRepository.CreateCatalogItemAsync(catalogItem);
+        return new CreateCatalogItemResult(catalogItem.Id);
+    }
+}
+
+// public class CreateCatalogItemHandler(
+//     ICatalogItemRepository catalogItemRepository,
+//     IBrandRepository brandRepository,
+//     ICategoryRepository categoryRepository)
+//     : IRequestHandler<CreateCatalogItemCommand, CreateCatalogItemResult>
+// {
+//     public async Task<CreateCatalogItemResult> Handle(CreateCatalogItemCommand command, 
+//         CancellationToken cancellationToken)
+//     {
+//         // Проверяем Brand
+//         var brandExists = command.Brand != null
+//             && (await brandRepository.GetAllBrandsAsync())
+//             .Any(b => b.Id == command?.Brand?.Id);
+
+//         if (!brandExists)
+//         {
+//             throw new ArgumentException("Указанный бренд не существует");
+//         }
+
+//         // Проверяем Category
+//         var categoryExists = command.Category != null
+//             && (await categoryRepository.GetAllCategoriesAsync())
+//             .Any(c => c.Id == command.Category.Id);
+
+//         if (!categoryExists)
+//         {
+//             throw new ArgumentException("Указанная категория не существует");
+//         }
+
+//         var catalogItem = command.Adapt<CatalogItem>();
+//         catalogItem.Id = Guid.NewGuid();
+//         await catalogItemRepository.CreateCatalogItemAsync(catalogItem);
+//         return new CreateCatalogItemResult(catalogItem.Id);
+//     }
+// }
